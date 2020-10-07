f4ee7ee5339ccda8f86af024bb99e533ea54ec28
leee1846 <leee1846@gmail.com>
2020-10-07 15:24:46 +0900

    still not working

--- 2020.10/Nexflix Clone/script.js
+++ 2020.10/Nexflix Clone/script.js
@@ -1,9 +1,12 @@
-let button = document.querySelector('.q-button')
+let button = document.querySelectorAll('.q-button')
 let appear = document.querySelectorAll('.question-box .bottom');
 
 button.forEach(addEventListener('click',(e)=>{  
   e.target.classList.toggle('active');
-  for (let i = 0; i < appear.length; i++) {
-      appear[i].classList.toggle('show');
+    if (appear.style.display === "none") {
+      appear.style.display = "block";
+    } else {
+      appear.style.display = "none";
+    }
   }
-}))
+))
