22857d4c90b65781eab421365c21f81393e58b13
leee1846 <leee1846@gmail.com>
2020-10-10 21:34:48 +0900

    fixed "+" toggle, size, appear

--- 2020.10/Nexflix Clone/script.js
+++ 2020.10/Nexflix Clone/script.js
@@ -2,18 +2,31 @@ let button = document.querySelectorAll('.q-button')
 let appear = document.querySelectorAll('.question-box > .bottom');
 
 
-for(let i = 0 ; i < button.length; i++){
-	button[i].addEventListener('click', function(e){
+for(let i = 0; i < button.length; i++) {
+	button[i].addEventListener('click',(e)=>{
 		e.target.classList.toggle('active');
-		if(e.target.classList.contains('active')){
-			appear[i].style.display = "block";
-		}else{
-			appear[i].style.display = "none"
+		if(e.target.classList.contains('active')) {
+			appear[i].style.display = 'block';
+		} else {
+			appear[i].style.display = 'none';
 		}
 	})
 }
 
-//
+
+
+// for(let i = 0 ; i < button.length; i++){
+// 	button[i].addEventListener('click', function(e){
+// 		e.target.classList.toggle('active');
+// 		if(e.target.classList.contains('active')){
+// 			appear[i].style.display = "block";
+// 		}else{
+// 			appear[i].style.display = "none"
+// 		}
+// 	})
+// }
+
+
 //button.forEach(addEventListener('click',(e)=>{  
 //  e.target.classList.toggle('active');
 //    if (appear.style.display === "none") {
