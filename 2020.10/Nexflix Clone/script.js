2dd7fd02ff92810bfb960627d0af1aa65c13001d
leee1846 <leee1846@gmail.com>
2020-10-10 18:32:23 +0900

    20201010

--- 2020.10/Nexflix Clone/script.js
+++ 2020.10/Nexflix Clone/script.js
@@ -1,12 +1,29 @@
 let button = document.querySelectorAll('.q-button')
-let appear = document.querySelectorAll('.question-box .bottom');
+let appear = document.querySelectorAll('.question-box > .bottom');
 
-button.forEach(addEventListener('click',(e)=>{  
-  e.target.classList.toggle('active');
-    if (appear.style.display === "none") {
-      appear.style.display = "block";
-    } else {
-      appear.style.display = "none";
-    }
-  }
-))
+
+for(let i = 0 ; i < button.length; i++){
+	button[i].addEventListener('click', function(e){
+		e.target.classList.toggle('active');
+		if(e.target.classList.contains('active')){
+			appear[i].style.display = "block";
+		}else{
+			appear[i].style.display = "none"
+		}
+	})
+}
+
+//
+//button.forEach(addEventListener('click',(e)=>{  
+//  e.target.classList.toggle('active');
+//    if (appear.style.display === "none") {
+//      appear.style.display = "block";
+//    } else {
+//      appear.style.display = "none";
+//    }
+//	
+//		for(var i = 0 ; i < 6; i++){
+//			console.log(button[i])
+//		}
+//  })
+//)
